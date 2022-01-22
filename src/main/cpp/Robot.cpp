e653a5edc51d070819546518647ed51c69035f3f
dylan-meiners <dylanmeiners@gmail.com>
2022-01-22 10:10:45 -0600

    Minimal driving code

--- src/main/cpp/Robot.cpp
+++ src/main/cpp/Robot.cpp
@@ -8,60 +8,63 @@
 #include <fmt/core.h>
 
 #include <frc/smartdashboard/SmartDashboard.h>
+#include <frc/Joystick.h>
 
 #include "swerve/src/include/SwerveTrain.h"
+#include "controller/Controller.h"
+#include "limelight/Limelight.h"
+
+frc::Joystick* playerOne;
 
 void Robot::RobotInit() {
-  m_chooser.SetDefaultOption(kAutoNameDefault, kAutoNameDefault);
-  m_chooser.AddOption(kAutoNameCustom, kAutoNameCustom);
-  frc::SmartDashboard::PutData("Auto Modes", &m_chooser);
+
+    playerOne = new frc::Joystick(R_controllerPortPlayerOne);
 }
 
-/**
- * This function is called every robot packet, no matter the mode. Use
- * this for items like diagnostics that you want ran during disabled,
- * autonomous, teleoperated and test.
- *
- * <p> This runs after the mode specific periodic functions, but before
- * LiveWindow and SmartDashboard integrated updating.
- */
 void Robot::RobotPeriodic() {}
 
-/**
- * This autonomous (along with the chooser code above) shows how to select
- * between different autonomous modes using the dashboard. The sendable chooser
- * code works with the Java SmartDashboard. If you prefer the LabVIEW Dashboard,
- * remove all of the chooser code and uncomment the GetString line to get the
- * auto name from the text box below the Gyro.
- *
- * You can add additional auto modes by adding additional comparisons to the
- * if-else structure below with additional strings. If using the SendableChooser
- * make sure to add them to the chooser code above as well.
- */
-void Robot::AutonomousInit() {
-  m_autoSelected = m_chooser.GetSelected();
-  // m_autoSelected = SmartDashboard::GetString("Auto Selector",
-  //     kAutoNameDefault);
-  fmt::print("Auto selected: {}\n", m_autoSelected);
+void Robot::AutonomousInit() {}
 
-  if (m_autoSelected == kAutoNameCustom) {
-    // Custom Auto goes here
-  } else {
-    // Default Auto goes here
-  }
-}
-
-void Robot::AutonomousPeriodic() {
-  if (m_autoSelected == kAutoNameCustom) {
-    // Custom Auto goes here
-  } else {
-    // Default Auto goes here
-  }
-}
+void Robot::AutonomousPeriodic() {}
 
 void Robot::TeleopInit() {}
 
-void Robot::TeleopPeriodic() {}
+void Robot::TeleopPeriodic() {
+
+    if (playerOne->GetRawButton(3)) {
+
+        SwerveTrain::GetInstance().SetZeroPosition();
+    }
+    if (playerOne->GetRawButton(4)) {
+
+        NavX::GetInstance().resetYaw();
+    }
+    if (playerOne->GetRawButton(12)) {
+        
+        SwerveTrain::GetInstance().AssumeZeroPosition();
+    }
+    else {
+
+        double x = playerOne->GetX();
+        double y = playerOne->GetY();
+        double z = playerOne->GetZ();
+        Controller::forceControllerXYZToZeroInDeadzone(x, y, z);
+
+        frc::SmartDashboard::PutNumber("x", x);
+        frc::SmartDashboard::PutNumber("y", y);
+        frc::SmartDashboard::PutNumber("z", z);
+
+        SwerveTrain::GetInstance().Drive(
+            -x,
+            -y,
+            playerOne->GetRawButton(6) ? Limelight::GetInstance().CalculateLimelightLockSpeed() : z,
+            playerOne->GetRawButton(5),
+            playerOne->GetRawButton(7),
+            playerOne->GetRawButton(2),
+            -(((playerOne->GetThrottle() + 1.0) / 2.0) - 1.0)
+        );
+    }
+}
 
 void Robot::DisabledInit() {}
 
@@ -73,6 +76,6 @@ void Robot::TestPeriodic() {}
 
 #ifndef RUNNING_FRC_TESTS
 int main() {
-  return frc::StartRobot<Robot>();
+    return frc::StartRobot<Robot>();
 }
 #endif
--- src/main/include/Robot.h
+++ src/main/include/Robot.h
@@ -10,21 +10,15 @@
 #include <frc/smartdashboard/SendableChooser.h>
 
 class Robot : public frc::TimedRobot {
- public:
-  void RobotInit() override;
-  void RobotPeriodic() override;
-  void AutonomousInit() override;
-  void AutonomousPeriodic() override;
-  void TeleopInit() override;
-  void TeleopPeriodic() override;
-  void DisabledInit() override;
-  void DisabledPeriodic() override;
-  void TestInit() override;
-  void TestPeriodic() override;
-
- private:
-  frc::SendableChooser<std::string> m_chooser;
-  const std::string kAutoNameDefault = "Default";
-  const std::string kAutoNameCustom = "My Auto";
-  std::string m_autoSelected;
+    public:
+        void RobotInit() override;
+        void RobotPeriodic() override;
+        void AutonomousInit() override;
+        void AutonomousPeriodic() override;
+        void TeleopInit() override;
+        void TeleopPeriodic() override;
+        void DisabledInit() override;
+        void DisabledPeriodic() override;
+        void TestInit() override;
+        void TestPeriodic() override;
 };
