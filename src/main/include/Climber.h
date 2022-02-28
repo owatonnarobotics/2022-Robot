ae8f4c3ffc92ed738c579ff1c291519efbe5c2d5
Aiden Engel <crunchman45@gmail.com>
2022-02-28 16:57:17 -0600

    Adjusted Climber code and initialized another Climber Motor

--- src/main/include/Climber.h
+++ src/main/include/Climber.h
@@ -9,18 +9,21 @@ class Climber {
 
     public:
         static Climber& GetInstance() {
-            static Climber* instance = new Climber(R_ClimberCANID);
+            static Climber* instance = new Climber(R_ClimberCANID, R_ClimberCANID2);
             return *instance;
         }
 
         void SetClimberSpeed(const double &speedToSet) {
             m_ClimberMotor->Set(ctre::phoenix::motorcontrol::ControlMode::PercentOutput, speedToSet);
+            m_ClimberMotor2->Set(ctre::phoenix::motorcontrol::ControlMode::PercentOutput, speedToSet);
         }
     
     private:
-        Climber(const int R_ClimberCANID) {
+        Climber(const int R_ClimberCANID, const int R_ClimberCANID2) {
             m_ClimberMotor = new ctre::phoenix::motorcontrol::can::VictorSPX(R_ClimberCANID);
+            m_ClimberMotor2 = new ctre::phoenix::motorcontrol::can::VictorSPX(R_ClimberCANID2);
         }
         
         ctre::phoenix::motorcontrol::can::VictorSPX *m_ClimberMotor;
+        ctre::phoenix::motorcontrol::can::VictorSPX *m_ClimberMotor2;
 };
